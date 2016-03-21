6bc766bc790991658c662784ccaa742d75bb3231
Christian Schneider <chris@die-schneider.net>
2016-03-21 13:21:33 +0100

    [ARIES-1509] Adding repository and fixing some issues

--- examples/echotcp/service/src/main/java/org/apache/aries/rsa/examples/echotcp/service/EchoServiceImpl.java
+++ examples/echotcp/service/src/main/java/org/apache/aries/rsa/examples/echotcp/service/EchoServiceImpl.java
@@ -3,7 +3,7 @@ package org.apache.aries.rsa.examples.echotcp.service;
 import org.apache.aries.rsa.examples.echotcp.api.EchoService;
 import org.osgi.service.component.annotations.Component;
 
-@Component(property={"service.exported.interfaces=*"})
+@Component(property={"service.exported.interfaces=*", "port=8201"})
 public class EchoServiceImpl implements EchoService {
 
     @Override
