fc5cd41085e6b9890972d422aa0379f1145f9364
Christian Schneider <chris@die-schneider.net>
2016-03-21 13:58:20 +0100

    [ARIES-1509] Use fixed port for example

--- examples/echotcp/service/src/main/java/org/apache/aries/rsa/examples/echotcp/service/EchoServiceImpl.java
+++ examples/echotcp/service/src/main/java/org/apache/aries/rsa/examples/echotcp/service/EchoServiceImpl.java
@@ -3,7 +3,9 @@ package org.apache.aries.rsa.examples.echotcp.service;
 import org.apache.aries.rsa.examples.echotcp.api.EchoService;
 import org.osgi.service.component.annotations.Component;
 
-@Component(property={"service.exported.interfaces=*", "port=8201"})
+@Component(property = {
+                       "service.exported.interfaces=*", "port=8201"
+})
 public class EchoServiceImpl implements EchoService {
 
     @Override
